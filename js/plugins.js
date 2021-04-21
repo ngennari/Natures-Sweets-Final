2fef3df51cfb51e85ccbd2ef9f680d15bffffd9c
Nick Gennari <nicholasgennari@yahoo.com>
2021-04-21 17:15:35 -0400

    Initial Commit

---
+++ js/plugins.js
@@ -0,0 +1,36 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
+
+// Place any jQuery/helper plugins in here.
+const btn = document.querySelector('.add');
+const overlay = document.querySelector('.overlay');
+
+btn.addEventListener('click',function(){
+  overlay.classList.remove('hide');
+  setTimeout(function(){overlay.classList.remove('fade')},100);
+})
+
+overlay.addEventListener('click',function(){
+  overlay.classList.add('fade');
+  setTimeout(function(){overlay.classList.add('hide')},200);
+})
